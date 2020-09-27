a2d62f5345e1e7c1294d03cd4a732f5ecd1bdc75
sferstein <57074114+sferstein@users.noreply.github.com>
2020-09-27 15:20:33 -0500

    Commit

---
+++ Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+    private int score;
+    public Text scoreText;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+        scoreText.text = "Score: " + 0;
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool isLowEnough = transform.position.y < 12;
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+        if(score >= 10)
+        {
+            gameOver = true;
+            scoreText.text = "You Win!\nPress R to Restart";
+        }
+        if(Input.GetKeyDown(KeyCode.R) && gameOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+            scoreText.text = "Game Over!\nPress R to Restart";
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+            score++;
+            scoreText.text = "Score: " + score;
+        }
+
+    }
+
+}
---
+++ Prototype3/Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public PlayerController playerController;
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+        if(playerController == null)
+        {
+            playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playerController.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+        if(playerController.gameOver && !won)
+        {
+            scoreText.text = "You Lose!" + "\n" + "Press R to Try Again!";
+        }
+        if(score >= 10)
+        {
+            playerController.gameOver = true;
+            won = true;
+
+            //playerController.StopRunning();
+
+            scoreText.text = "You Win!" + "\n" + "Press R to Try Again!";
+        }
+        if(playerController.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
